cf09de407524fab17c6ec46da4b91747749b08c1
Bertrand Caron <bertrand.caron@bnf.fr>
2025-05-23 10:47:01 +0000

    Added mag4 to requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import mag4 as mg
 
-st.title("🎈 My new app")
+st.title("Hello there")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
